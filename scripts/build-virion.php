e7243b740dd3e4b7c68a420a7600efb4a3ddb620
Vega Nicholas S. <veganicholas123@gmail.com>
2025-02-26 21:38:15 +0700

    Final fix

--- scripts/build-virion.php
+++ scripts/build-virion.php
@@ -72,8 +72,8 @@ foreach($iterator as $file){
 		$className = basename($file->getPathname());
 
 		if($namespace !== null){
-			$namespacePath = "src/" . str_replace("\\", "/", $namespace);
-			$pharPath = "$namespacePath/$className";
+			$namespacePath = str_replace("\\", "/", $namespace);
+			$pharPath = "src/$namespacePath/$className";
 		}else{
 			$relativePath = str_replace("$basePath/src/", "", $file->getPathname());
 			$pharPath = "src/$relativePath";
